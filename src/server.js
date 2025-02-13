009d1d7a7d02024495bad9d4b45b23ee557de93e
Digital-Jugglers <teamdjwebsite@gmail.com>
2025-02-13 13:57:05 +0530

    chrome update

--- src/server.js
+++ src/server.js
@@ -25,7 +25,12 @@ app.use(
     origin: [
       "https://seo-dashboard-client.digitalmarketinglucknow.com",
       "https://seo-dashboard-admin.digitalmarketinglucknow.com",
+      "https://seo-server-7icx.onrender.com",
       "http://localhost:3000",
+      "http://localhost:3001",
+      "52.41.36.82",
+      "54.191.253.12",
+      "44.226.122.3",
     ],
   })
 );
